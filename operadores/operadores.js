d47eaad19af28f88b12da3ac3c8bbe889c7efe48
kethy <134504091+kethely1@users.noreply.github.com>
2023-09-19 08:45:50 -0300

    Add files via upload

---
+++ operadores/operadores.js
@@ -0,0 +1,35 @@
+
+    function soma(){
+    var s1= document.getElementByI("valor1").value;
+    var s2= document.getElementById("valor2").value;
+    var result =parseInt(s1) + parseInt(s2);
+   document.getElementById("resultado"). innerHTML = result
+ 
+}
+    
+    function subtrair(){s
+        var s1= document.getElementByI("valor1").value;
+        var s2= document.getElementById("valor2").value;
+        var result =parseInt(s1) - parseInt(s2);
+        document.getElementById("resultado"). innerHTML = result
+ 
+        
+    function multiplica(){
+        var s1= document.getElementByI("valor1").value;
+        var s2= document.getElementById("valor2").value;
+        var result=parseInt(s1) * parseInt(s2);
+        document.getElementById("resultado"). innerHTML = result
+    }
+ 
+        
+    function dividir(){
+        var s1= document.getElementByI("valor1").value;
+        var s2= document.getElementById("valor2").value;
+        var result =parseInt(s1) /parseInt (s2);
+        document.getElementById("resultado"). innerHTML = result
+    }
+     
+
+    
+
+
